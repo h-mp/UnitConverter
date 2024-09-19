6a57c7d32fa8e1ec9ae9a3a5d1e16b8b6f2252a1
Hilja-Maria Paananen <143746544+h-mp@users.noreply.github.com>
2024-09-19 21:10:58 +0300

    Write tests for the WeightConverter

---
+++ tests/WeightConverter.test.js
@@ -0,0 +1,48 @@
+/**
+ * Tests for the WeightConverter class.
+ * 
+ * @author Hilja-Maria Paananen <hp222qn@student.lnu.se>
+ */
+
+import { WeightConverter } from "../src/WeightConverter.js"
+
+const weightConverter = new WeightConverter()
+
+// Test case to check kilogram to pound conversion
+test('Converting kilograms to pounds', () => {
+  expect(weightConverter.kilogramsToPounds(5)).toBeCloseTo(11.02, 2)
+
+  expect(weightConverter.kilogramsToPounds(21)).toBeCloseTo(46.297, 2)
+})
+
+// Test case to check pound to kilogram conversion
+test('Converting pounds to kilograms', () => {
+  expect(weightConverter.poundsToKilograms(14)).toBeCloseTo(6.35, 2)
+
+  expect(weightConverter.poundsToKilograms(1.5)).toBeCloseTo(0.68, 2)
+})
+
+// Test case to check gram to ounce conversion
+test('Converting grams to ounces', () => {
+  expect(weightConverter.gramsToOunces(119)).toBeCloseTo(4.197, 2)
+
+  expect(weightConverter.gramsToOunces(567)).toBeCloseTo(20, 2)
+})
+
+// Test case to check ounce to gram conversion
+test('Converting ounces to grams', () => {
+  expect(weightConverter.ouncesToGrams(16)).toBeCloseTo(453.59, 2)
+
+  expect(weightConverter.ouncesToGrams(5.5)).toBeCloseTo(155.92, 2)
+})
+
+// Test case to check input validation for both methods
+test('Input validation', () => {
+  expect(() => weightConverter.kilogramsToPounds('string')).toThrowError('Weight must be a number')
+
+  expect(() => weightConverter.kilogramsToPounds(null)).toThrowError('Weight must be a number')
+
+  expect(() => weightConverter.poundsToKilograms(NaN)).toThrowError('Weight must be a number')
+
+  expect(() => weightConverter.poundsToKilograms(undefined)).toThrowError('Weight must be a number')
+})
