5a8a2f714a587258384bbf1aa0872a529f6d7036
Hilja-Maria Paananen <143746544+h-mp@users.noreply.github.com>
2024-09-19 18:25:19 +0300

    Create the TemperatureConverter class

---
+++ src/TemperatureConverter.js
@@ -0,0 +1,47 @@
+/**
+ * Module for the temperature converter class.
+ * 
+ * @author Hilja-Maria Paananen <hp222qn@student.lnu.se>
+ */
+
+/**
+ * A class to convert temperatures
+ */
+export class TemperatureConverter {
+  /**
+   * Validates the input.
+   *
+   * @param {Number} temperature - The temperature to be converted
+   */
+  #inputValidation(temperature) {
+    if (typeof temperature !== 'number' || isNaN(temperature)) {
+      throw new Error('Temperature must be a number')
+    }
+  }
+
+  /**
+   * Converts the temperature from Fahrenheit to Celsius.
+   *
+   * @param {Number} fahrenheit - The temperature in Fahrenheit
+   * @returns {Number} - The temperature in Celsius
+   */
+  fahrenheitToCelsius(fahrenheit) {
+    this.#inputValidation(fahrenheit)
+
+    const celsius = (fahrenheit - 32) / 1.8
+    return celsius
+  }
+
+  /**
+   * Converts the temperature from Celsius to Fahrenheit.
+   *
+   * @param {Number} celsius - The temperature in Celsius
+   * @return {Number} - The temperature in Fahrenheit
+   */
+  celsiusToFahrenheit(celsius) {
+    this.#inputValidation(celsius)
+
+    const fahrenheit = (celsius * 1.8) + 32
+    return fahrenheit
+  }
+}
