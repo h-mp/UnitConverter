8c57317d78c17ae357706ac4180c10c82a033621
Hilja-Maria Paananen <143746544+h-mp@users.noreply.github.com>
2024-09-20 21:34:54 +0300

    Write tests for cm to in conversion

--- tests/LengthConverter.test.js
+++ tests/LengthConverter.test.js
@@ -19,16 +19,30 @@ test('Converting meters to feet', () => {
 test('Converting feet to meters', () => {
   expect(lengthConverter.feetToMeters(40)).toBeCloseTo(12.19,2)
 
-  expect(lengthConverter.feetToMeters(151.4)).toBeCloseTo(46.146,2)
+  expect(lengthConverter.feetToMeters(151.4)).toBeCloseTo(46.146, 2)
+})
+
+// Test case to check centimeters to inches conversion
+test('Converting centimeters to inches', () => {
+  expect(lengthConverter.centimetersToInches(16.7)).toBeCloseTo(6.574, 2)
+
+  expect(lengthConverter.centimetersToInches(159)).toBeCloseTo(62.598, 2)
+})
+
+// Test case to check inches to centimeters conversion
+test('Converting inches to centimeters', () => {
+  expect(lengthConverter.inchesToCentimeters(28)).toBeCloseTo(71.12, 2)
+
+  expect(lengthConverter.inchesToCentimeters(0.5)).toBeCloseTo(1.27, 2)
 })
 
 // Test case to check input validation for all methods
 test('Input validation', () => {
   expect(() => lengthConverter.metersToFeet('string')).toThrowError('Length must be a number')
 
-  expect(() => lengthConverter.metersToFeet(NaN)).toThrowError('Length must be a number')
+  expect(() => lengthConverter.feetToMeters(NaN)).toThrowError('Length must be a number')
 
-  expect(() => lengthConverter.feetToMeters(undefined)).toThrowError('Length must be a number')
+  expect(() => lengthConverter.centimetersToInches(undefined)).toThrowError('Length must be a number')
 
-  expect(() => lengthConverter.feetToMeters(null)).toThrowError('Length must be a number')
+  expect(() => lengthConverter.inchesToCentimeters(null)).toThrowError('Length must be a number')
 })
