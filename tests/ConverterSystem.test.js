f0dd2709c512e0a759d2598b75580218065dd5e1
Hilja-Maria Paananen <143746544+h-mp@users.noreply.github.com>
2024-09-28 23:13:51 +0300

    Write tests for ConverterSystem length conversion

--- tests/ConverterSystem.test.js
+++ tests/ConverterSystem.test.js
@@ -8,6 +8,34 @@ import { ConverterSystem } from '../src/ConverterSystem'
 
 const converterSystem = new ConverterSystem()
 
+// Test case to check length conversion from meters
+test('Converting length from meters', () => {
+  expect(converterSystem.convertLength('m', 'feet', 2)).toBeCloseTo(6.561, 2)
+
+  expect(() => converterSystem.convertLength('meters', 'in', 21)).toThrowError('Conversion not available')
+})
+
+// Test case to check length conversion from feet
+test('Converting length from feet', () => {
+  expect(converterSystem.convertLength('ft', 'meters', 23)).toBeCloseTo(7.010, 2)
+
+  expect(() => converterSystem.convertLength('feet', 'cm', 93)).toThrowError('Conversion not available')
+})
+
+// Test case to check length conversion from centimeters
+test('Converting length from centimeters', () => {
+  expect(converterSystem.convertLength('cm', 'inches', 70)).toBeCloseTo(27.559, 2)
+
+  expect(() => converterSystem.convertLength('centimeters', 'm', 55)).toThrowError('Conversion not available')
+})
+
+// Test case to check length conversion from inches
+test('Converting length from inches', () => {
+  expect(converterSystem.convertLength('in', 'centimeters', 9)).toBeCloseTo(22.86, 2)
+
+  expect(() => converterSystem.convertLength('inches', 'feet', 24)).toThrowError('Conversion not available')
+})
+
 // Test case to check weight conversion from kilograms 
 test('Converting weight from kilograms', () => {
   expect(converterSystem.convertWeight('kg', 'Pounds', 12)).toBeCloseTo(26.455, 2)
