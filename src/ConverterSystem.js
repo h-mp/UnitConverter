0120919605301bb002e0c92caf9f14d5f3f4016a
Hilja-Maria Paananen <143746544+h-mp@users.noreply.github.com>
2024-09-28 20:51:23 +0300

    Implement weight conversion in ConverterSystem

--- src/ConverterSystem.js
+++ src/ConverterSystem.js
@@ -4,7 +4,7 @@
  * @author Hilja-Maria Paananen <hp222qn@student.lnu.se>
  */
 
-import { InputValidator } from '../InputValidator.js'
+import { InputValidator } from './InputValidator.js'
 import { LengthConverter } from './converters/LengthConverter.js'
 import { TemperatureConverter } from './converters/TemperatureConverter.js'
 import { VolumeConverter } from './converters/VolumeConverter.js'
@@ -30,5 +30,105 @@ export class ConverterSystem {
     this.#temperatureConverter = new TemperatureConverter()
     this.#volumeConverter = new VolumeConverter()
     this.#weightConverter = new WeightConverter()
+  }
+
+  /**
+   * Handles weight conversion selection.
+   *
+   * @param {String} convertFrom - The unit to convert from
+   * @param {String} convertTo - The unit to convert to
+   * @param {Number} numberToConvert - The number to convert
+   * @throws {Error} - If the conversion is not available
+   * @returns {Number} - The converted number
+   */
+  convertWeight(convertFrom, convertTo, numberToConvert) {
+    switch (convertFrom.toLowerCase()) {
+      case 'kilograms':
+      case 'kg':
+        return this.#convertFromKilograms(convertTo, numberToConvert)
+      case 'pounds':
+      case 'lb':
+        return this.#convertFromPounds(convertTo, numberToConvert)
+      case 'grams':
+      case 'g':
+        return this.#convertFromGrams(convertTo, numberToConvert)
+      case 'ounces':
+      case 'oz':
+        return this.#convertFromOunces(convertTo, numberToConvert)
+      default:
+        throw new Error('Conversion not available')
+      }
+    }
+
+  /**
+   * Converts the kilograms to the selected unit.
+   * 
+   * @param {String} convertTo - The unit to convert to
+   * @param {Number} numberToConvert - The number to convert
+   * @throws {Error} - If the conversion is not available
+   * @returns {Number} - The converted number
+   */
+  #convertFromKilograms(convertTo, numberToConvert) {
+    switch(convertTo.toLowerCase()) {
+      case 'pounds':
+      case 'lb':
+        return this.#weightConverter.kilogramsToPounds(numberToConvert)
+      default:
+        throw new Error('Conversion not available')
+    }
+  }
+
+  /**
+   * Converts the pounds to the selected unit.
+   * 
+   * @param {String} convertTo - The unit to convert to
+   * @param {Number} numberToConvert - The number to convert
+   * @throws {Error} - If the conversion is not available
+   * @returns {Number} - The converted number
+   */
+  #convertFromPounds(convertTo, numberToConvert) {
+    switch(convertTo.toLowerCase()) {
+      case 'kilograms':
+      case 'kg':
+        return this.#weightConverter.poundsToKilograms(numberToConvert)
+      default:
+        throw new Error('Conversion not available')
+    }
+  }
+
+  /**
+   * Converts the grams to the selected unit.
+   * 
+   * @param {String} convertTo - The unit to convert to
+   * @param {Number} numberToConvert - The number to convert
+   * @throws {Error} - If the conversion is not available
+   * @returns {Number} - The converted number
+   */
+  #convertFromGrams(convertTo, numberToConvert) {
+    switch(convertTo.toLowerCase()) {
+      case 'ounces':
+      case 'oz':
+        return this.#weightConverter.gramsToOunces(numberToConvert)
+      default:
+        throw new Error('Conversion not available')
+    }
+  }
+
+  /**
+   * Converts the ounces to the selected unit.
+   * 
+   * @param {String} convertTo - The unit to convert to
+   * @param {Number} numberToConvert - The number to convert
+   * @throws {Error} - If the conversion is not available
+   * @returns {Number} - The converted number
+   */
+  #convertFromOunces(convertTo, numberToConvert) {
+    switch(convertTo.toLowerCase()) {
+      case 'grams':
+      case 'g':
+        return this.#weightConverter.ouncesToGrams(numberToConvert)
+      default:
+        throw new Error('Conversion not available')
+    }
   }
 }
