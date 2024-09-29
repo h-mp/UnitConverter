0ad38b9495335699669ccaeb9dbacbdf2a2213c4
Hilja-Maria Paananen <143746544+h-mp@users.noreply.github.com>
2024-09-29 18:21:30 +0300

    Create methods for temperature range validation

--- src/InputValidator.js
+++ src/InputValidator.js
@@ -38,9 +38,33 @@ export class InputValidator {
    * @param {Number} input - The input
    * @throws {Error} - If the input is not a number
    */
-    validatePositiveNumber(input) {
-      if (input < 0) {
-        throw new Error('Number must be positive')
-      }
+  validatePositiveNumber(input) {
+    if (input < 0) {
+      throw new Error('Number must be positive')
     }
+  }
+
+  /**
+   * Validates the Celsius temperature range.
+   * 
+   * @param {Number} celsius - The temperature in celsius
+   * @throws {Error} - If the temperature is below absolute zero 
+   */
+  validateCelsiusRange(celsius) {
+    if (celsius < -273.15) {
+      throw new Error('Temperature must be greater than or equal to -273.15°C')
+    }
+  }
+
+  /**
+   * Validates the Fahrenheit temperature range.
+   * 
+   * @param {Number} fahrenheit - The temperature in fahrenheit
+   * @throws {Error} - If the temperature is below absolute zero 
+   */
+  validateFahrenheitRange(fahrenheit) {
+    if (fahrenheit < -459.67) {
+      throw new Error('Temperature must be greater than or equal to -459.67°F')
+    }
+  }
 }
