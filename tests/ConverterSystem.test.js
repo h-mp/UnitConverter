611c6b1720b2674c71fca2d9dcdc5d3ddb43798e
Hilja-Maria Paananen <143746544+h-mp@users.noreply.github.com>
2024-09-29 19:02:49 +0300

    Write tests for input validation in ConverterSystem

--- tests/ConverterSystem.test.js
+++ tests/ConverterSystem.test.js
@@ -14,6 +14,10 @@ const converterSystem = new ConverterSystem()
 test('Converting temperature from Celsius', () => {
   expect(converterSystem.convertTemperature('c', 'fahrenheit', 18)).toBe(64.4)
 
+  expect(converterSystem.convertTemperature('celsius', 'fahrenheit', -25)).toBe(-13)
+
+  expect(() => converterSystem.convertTemperature('c', 'f', -300)).toThrowError('Temperature must be greater than or equal to -273.15°C')
+
   expect(() => converterSystem.convertTemperature('celsius', 'kelvin', 21)).toThrowError('Conversion not available')
 })
 
@@ -21,7 +25,22 @@ test('Converting temperature from Celsius', () => {
 test('Converting temperature from Fahrenheit', () => {
   expect(converterSystem.convertTemperature('f', 'celsius', 68)).toBe(20)
 
+  expect(converterSystem.convertTemperature('fahrenheit', 'celsius', -22)).toBe(-30)
+
+  expect(() => converterSystem.convertTemperature('f', 'c', -500)).toThrowError('Temperature must be greater than or equal to -459.67°F')
+
   expect(() => converterSystem.convertTemperature('fahrenheit', 'k', 97)).toThrowError('Conversion not available')
+})
+
+// Test case to check input validation in temperature conversion
+test('Check input validation in temperature conversion', () => {
+  expect(() => converterSystem.convertTemperature('f', 'celsius', 'string')).toThrowError('Input must be a number')
+
+  expect(() => converterSystem.convertTemperature('f', 'celsius', NaN)).toThrowError('Input must be a number')
+
+  expect(() => converterSystem.convertTemperature(2, 'celsius', 3)).toThrowError('Input must be a string')
+
+  expect(() => converterSystem.convertTemperature('f', [], 3)).toThrowError('Input must be a string')
 })
 
 // Test cases for length conversion
@@ -54,6 +73,19 @@ test('Converting length from inches', () => {
   expect(() => converterSystem.convertLength('inches', 'feet', 24)).toThrowError('Conversion not available')
 })
 
+// Test case to check input validation in length conversion
+test('Check input validation in length conversion', () => {
+  expect(() => converterSystem.convertVolume('m', 'ft', -10)).toThrowError('Number must be positive')
+
+  expect(() => converterSystem.convertLength('ft', 'm', 'string')).toThrowError('Input must be a number')
+
+  expect(() => converterSystem.convertLength('ft', 'm', NaN)).toThrowError('Input must be a number')
+
+  expect(() => converterSystem.convertLength(2, 'inches', 3)).toThrowError('Input must be a string')
+
+  expect(() => converterSystem.convertLength('m', [], 3)).toThrowError('Input must be a string')
+})
+
 // Test cases for weight conversion
 
 // Test case to check weight conversion from kilograms 
@@ -82,6 +114,19 @@ test('Converting weight from ounces', () => {
   expect(converterSystem.convertWeight('oz', 'Grams', 7)).toBeCloseTo(198.446, 2)
 
   expect(() => converterSystem.convertWeight('ounces', 'kilograms', 3)).toThrowError('Conversion not available')
+})
+
+// Test case to check input validation in weight conversion
+test('Check input validation in weight conversion', () => {
+  expect(() => converterSystem.convertVolume('lb', 'kg', -10)).toThrowError('Number must be positive')
+
+  expect(() => converterSystem.convertWeight('kg', 'pounds', 'string')).toThrowError('Input must be a number')
+
+  expect(() => converterSystem.convertWeight('kg', 'lb', NaN)).toThrowError('Input must be a number')
+
+  expect(() => converterSystem.convertWeight(2, 'g', 3)).toThrowError('Input must be a string')
+
+  expect(() => converterSystem.convertWeight('oz', [], 3)).toThrowError('Input must be a string')
 })
 
 // Test cases for volume conversion
@@ -121,4 +166,17 @@ test('Converting volume from cups', () => {
   expect(converterSystem.convertVolume('c', 'deciliters', 25)).toBeCloseTo(59.147, 2)
 
   expect(() => converterSystem.convertVolume('cups', 'liters', 17)).toThrowError('Conversion not available')
+})
+
+// Test case to check input validation in volume conversion
+test('Check input validation in volume conversion', () => {
+  expect(() => converterSystem.convertVolume('l', 'gal', -10)).toThrowError('Number must be positive')
+
+  expect(() => converterSystem.convertVolume('l', 'gal', 'string')).toThrowError('Input must be a number')
+
+  expect(() => converterSystem.convertVolume('dl', 'cups', NaN)).toThrowError('Input must be a number')
+
+  expect(() => converterSystem.convertVolume(2, 'l', 3)).toThrowError('Input must be a string')
+
+  expect(() => converterSystem.convertVolume('pints', [], 3)).toThrowError('Input must be a string')
 })
