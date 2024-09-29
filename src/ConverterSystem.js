629d3d379e383656cbd564c99360d0e899ed3bed
Hilja-Maria Paananen <143746544+h-mp@users.noreply.github.com>
2024-09-29 19:43:07 +0300

    Create a ConverterSelector class and move private methods there from ConverterSystem

--- src/ConverterSystem.js
+++ src/ConverterSystem.js
@@ -5,28 +5,16 @@
  */
 
 import { InputValidator } from './InputValidator.js'
-import { LengthConverter } from './converters/LengthConverter.js'
-import { TemperatureConverter } from './converters/TemperatureConverter.js'
-import { VolumeConverter } from './converters/VolumeConverter.js'
-import { WeightConverter } from './converters/WeightConverter.js'
+import { ConverterSelector } from './ConverterSelector.js'
 
 export class ConverterSystem {
   #inputValidator
 
-  #lengthConverter
-
-  #temperatureConverter
-
-  #volumeConverter
-
-  #weightConverter
+  #converterSelector
 
   constructor() {
     this.#inputValidator = new InputValidator()
-    this.#lengthConverter = new LengthConverter()
-    this.#temperatureConverter = new TemperatureConverter()
-    this.#volumeConverter = new VolumeConverter()
-    this.#weightConverter = new WeightConverter()
+    this.#converterSelector = new ConverterSelector()
   }
 
   /**
@@ -58,11 +46,13 @@ export class ConverterSystem {
       case 'celsius':
       case 'c':
         this.#inputValidator.validateCelsiusRange(numberToConvert)
-        return this.#convertFromCelsius(convertTo, numberToConvert)
+
+        return this.#converterSelector.convertFromCelsius(convertTo, numberToConvert)
       case 'fahrenheit':
       case 'f':
         this.#inputValidator.validateFahrenheitRange(numberToConvert)
-        return this.#convertFromFahrenheit(convertTo, numberToConvert)
+
+        return this.#converterSelector.convertFromFahrenheit(convertTo, numberToConvert)
       default:
         throw new Error('Conversion not available')
       }
@@ -84,16 +74,16 @@ export class ConverterSystem {
     switch (convertFrom.toLowerCase()) {
       case 'meters':
       case 'm':
-        return this.#convertFromMeters(convertTo, numberToConvert)
+        return this.#converterSelector.convertFromMeters(convertTo, numberToConvert)
       case 'feet':
       case 'ft':
-        return this.#convertFromFeet(convertTo, numberToConvert)
+        return this.#converterSelector.convertFromFeet(convertTo, numberToConvert)
       case 'centimeters':
       case 'cm':
-        return this.#convertFromCentimeters(convertTo, numberToConvert)
+        return this.#converterSelector.convertFromCentimeters(convertTo, numberToConvert)
       case 'inches':
       case 'in':
-        return this.#convertFromInches(convertTo, numberToConvert)
+        return this.#converterSelector.convertFromInches(convertTo, numberToConvert)
       default:
         throw new Error('Conversion not available')
       }
@@ -115,16 +105,16 @@ export class ConverterSystem {
     switch (convertFrom.toLowerCase()) {
       case 'kilograms':
       case 'kg':
-        return this.#convertFromKilograms(convertTo, numberToConvert)
+        return this.#converterSelector.convertFromKilograms(convertTo, numberToConvert)
       case 'pounds':
       case 'lb':
-        return this.#convertFromPounds(convertTo, numberToConvert)
+        return this.#converterSelector.convertFromPounds(convertTo, numberToConvert)
       case 'grams':
       case 'g':
-        return this.#convertFromGrams(convertTo, numberToConvert)
+        return this.#converterSelector.convertFromGrams(convertTo, numberToConvert)
       case 'ounces':
       case 'oz':
-        return this.#convertFromOunces(convertTo, numberToConvert)
+        return this.#converterSelector.convertFromOunces(convertTo, numberToConvert)
       default:
         throw new Error('Conversion not available')
       }
@@ -146,294 +136,21 @@ export class ConverterSystem {
     switch (convertFrom.toLowerCase()) {
       case 'liters':
       case 'l':
-        return this.#convertFromLiters(convertTo, numberToConvert)
+        return this.#converterSelector.convertFromLiters(convertTo, numberToConvert)
       case 'gallons':
       case 'gal':
-        return this.#convertFromGallons(convertTo, numberToConvert)
+        return this.#converterSelector.convertFromGallons(convertTo, numberToConvert)
       case 'pints':
       case 'pt':
-        return this.#convertFromPints(convertTo, numberToConvert)
+        return this.#converterSelector.convertFromPints(convertTo, numberToConvert)
       case 'deciliters':
       case 'dl':
-        return this.#convertFromDeciliters(convertTo, numberToConvert)
+        return this.#converterSelector.convertFromDeciliters(convertTo, numberToConvert)
       case 'cups':
       case 'c':
-        return this.#convertFromCups(convertTo, numberToConvert)
+        return this.#converterSelector.convertFromCups(convertTo, numberToConvert)
       default:
         throw new Error('Conversion not available')
       }
     }
-
-  /**
-   * Converts the temperature in Celsius to the selected unit.
-   * 
-   * @param {String} convertTo - The unit to convert to
-   * @param {Number} numberToConvert - The number to convert
-   * @throws {Error} - If the conversion is not available
-   * @returns {Number} - The converted number
-   */
-  #convertFromCelsius(convertTo, numberToConvert) {
-    switch(convertTo.toLowerCase()) {
-      case 'fahrenheit':
-      case 'f':
-        return this.#temperatureConverter.celsiusToFahrenheit(numberToConvert)
-      default:
-        throw new Error('Conversion not available')
-    }
-  }
-
-  /**
-   * Converts the temperature in Fahrenheit to the selected unit.
-   * 
-   * @param {String} convertTo - The unit to convert to
-   * @param {Number} numberToConvert - The number to convert
-   * @throws {Error} - If the conversion is not available
-   * @returns {Number} - The converted number
-   */
-  #convertFromFahrenheit(convertTo, numberToConvert) {
-    switch(convertTo.toLowerCase()) {
-      case 'celsius':
-      case 'c':
-        return this.#temperatureConverter.fahrenheitToCelsius(numberToConvert)
-      default:
-        throw new Error('Conversion not available')
-    }
-  }
-
-  /**
-   * Converts the meters to the selected unit.
-   * 
-   * @param {String} convertTo - The unit to convert to
-   * @param {Number} numberToConvert - The number to convert
-   * @throws {Error} - If the conversion is not available
-   * @returns {Number} - The converted number
-   */
-  #convertFromMeters(convertTo, numberToConvert) {
-    switch(convertTo.toLowerCase()) {
-      case 'feet':
-      case 'ft':
-        return this.#lengthConverter.metersToFeet(numberToConvert)
-      default:
-        throw new Error('Conversion not available')
-    }
-  }
-
-  /**
-   * Converts the feet to the selected unit.
-   * 
-   * @param {String} convertTo - The unit to convert to
-   * @param {Number} numberToConvert - The number to convert
-   * @throws {Error} - If the conversion is not available
-   * @returns {Number} - The converted number
-   */
-  #convertFromFeet(convertTo, numberToConvert) {
-    switch(convertTo.toLowerCase()) {
-      case 'meters':
-      case 'm':
-        return this.#lengthConverter.feetToMeters(numberToConvert)
-      default:
-        throw new Error('Conversion not available')
-    }
-  }
-
-  /**
-   * Converts the centimeters to the selected unit.
-   * 
-   * @param {String} convertTo - The unit to convert to
-   * @param {Number} numberToConvert - The number to convert
-   * @throws {Error} - If the conversion is not available
-   * @returns {Number} - The converted number
-   */
-  #convertFromCentimeters(convertTo, numberToConvert) {
-    switch(convertTo.toLowerCase()) {
-      case 'inches':
-      case 'in':
-        return this.#lengthConverter.centimetersToInches(numberToConvert)
-      default:
-        throw new Error('Conversion not available')
-    }
-  }
-
-  /**
-   * Converts the inches to the selected unit.
-   * 
-   * @param {String} convertTo - The unit to convert to
-   * @param {Number} numberToConvert - The number to convert
-   * @throws {Error} - If the conversion is not available
-   * @returns {Number} - The converted number
-   */
-  #convertFromInches(convertTo, numberToConvert) {
-    switch(convertTo.toLowerCase()) {
-      case 'centimeters':
-      case 'cm':
-        return this.#lengthConverter.inchesToCentimeters(numberToConvert)
-      default:
-        throw new Error('Conversion not available')
-    }
-  }
-
-  /**
-   * Converts the kilograms to the selected unit.
-   * 
-   * @param {String} convertTo - The unit to convert to
-   * @param {Number} numberToConvert - The number to convert
-   * @throws {Error} - If the conversion is not available
-   * @returns {Number} - The converted number
-   */
-  #convertFromKilograms(convertTo, numberToConvert) {
-    switch(convertTo.toLowerCase()) {
-      case 'pounds':
-      case 'lb':
-        return this.#weightConverter.kilogramsToPounds(numberToConvert)
-      default:
-        throw new Error('Conversion not available')
-    }
-  }
-
-  /**
-   * Converts the pounds to the selected unit.
-   * 
-   * @param {String} convertTo - The unit to convert to
-   * @param {Number} numberToConvert - The number to convert
-   * @throws {Error} - If the conversion is not available
-   * @returns {Number} - The converted number
-   */
-  #convertFromPounds(convertTo, numberToConvert) {
-    switch(convertTo.toLowerCase()) {
-      case 'kilograms':
-      case 'kg':
-        return this.#weightConverter.poundsToKilograms(numberToConvert)
-      default:
-        throw new Error('Conversion not available')
-    }
-  }
-
-  /**
-   * Converts the grams to the selected unit.
-   * 
-   * @param {String} convertTo - The unit to convert to
-   * @param {Number} numberToConvert - The number to convert
-   * @throws {Error} - If the conversion is not available
-   * @returns {Number} - The converted number
-   */
-  #convertFromGrams(convertTo, numberToConvert) {
-    switch(convertTo.toLowerCase()) {
-      case 'ounces':
-      case 'oz':
-        return this.#weightConverter.gramsToOunces(numberToConvert)
-      default:
-        throw new Error('Conversion not available')
-    }
-  }
-
-  /**
-   * Converts the ounces to the selected unit.
-   * 
-   * @param {String} convertTo - The unit to convert to
-   * @param {Number} numberToConvert - The number to convert
-   * @throws {Error} - If the conversion is not available
-   * @returns {Number} - The converted number
-   */
-  #convertFromOunces(convertTo, numberToConvert) {
-    switch(convertTo.toLowerCase()) {
-      case 'grams':
-      case 'g':
-        return this.#weightConverter.ouncesToGrams(numberToConvert)
-      default:
-        throw new Error('Conversion not available')
-    }
-  }
-
-  /**
-   * Converts the liters to the selected unit.
-   * 
-   * @param {String} convertTo - The unit to convert to
-   * @param {Number} numberToConvert - The number to convert
-   * @throws {Error} - If the conversion is not available
-   * @returns {Number} - The converted number
-   */
-  #convertFromLiters(convertTo, numberToConvert) {
-    switch(convertTo.toLowerCase()) {
-      case 'gallons':
-      case 'gal':
-        return this.#volumeConverter.litersToGallons(numberToConvert)
-      case 'pints':
-      case 'pt':
-        return this.#volumeConverter.litersToPints(numberToConvert)
-      default:
-        throw new Error('Conversion not available')
-    }
-  }
-
-  /**
-   * Converts the gallons to the selected unit.
-   * 
-   * @param {String} convertTo - The unit to convert to
-   * @param {Number} numberToConvert - The number to convert
-   * @throws {Error} - If the conversion is not available
-   * @returns {Number} - The converted number
-   */
-  #convertFromGallons(convertTo, numberToConvert) {
-    switch(convertTo.toLowerCase()) {
-      case 'liters':
-      case 'l':
-        return this.#volumeConverter.gallonsToLiters(numberToConvert)
-      default:
-        throw new Error('Conversion not available')
-    }
-  }
-
-  /**
-   * Converts the pints to the selected unit.
-   * 
-   * @param {String} convertTo - The unit to convert to
-   * @param {Number} numberToConvert - The number to convert
-   * @throws {Error} - If the conversion is not available
-   * @returns {Number} - The converted number
-   */
-  #convertFromPints(convertTo, numberToConvert) {
-    switch(convertTo.toLowerCase()) {
-      case 'liters':
-      case 'l':
-        return this.#volumeConverter.pintsToLiters(numberToConvert)
-      default:
-        throw new Error('Conversion not available')
-    }
-  }
-
-  /**
-   * Converts the deciliters to the selected unit.
-   * 
-   * @param {String} convertTo - The unit to convert to
-   * @param {Number} numberToConvert - The number to convert
-   * @throws {Error} - If the conversion is not available
-   * @returns {Number} - The converted number
-   */
-  #convertFromDeciliters(convertTo, numberToConvert) {
-    switch(convertTo.toLowerCase()) {
-      case 'cups':
-      case 'c':
-        return this.#volumeConverter.decilitersToCups(numberToConvert)
-      default:
-        throw new Error('Conversion not available')
-    }
-  }
-
-  /**
-   * Converts the cups to the selected unit.
-   * 
-   * @param {String} convertTo - The unit to convert to
-   * @param {Number} numberToConvert - The number to convert
-   * @throws {Error} - If the conversion is not available
-   * @returns {Number} - The converted number
-   */
-  #convertFromCups(convertTo, numberToConvert) {
-    switch(convertTo.toLowerCase()) {
-      case 'deciliters':
-      case 'dl':
-        return this.#volumeConverter.cupsToDeciliters(numberToConvert)
-      default:
-        throw new Error('Conversion not available')
-    }
-  }
 }
