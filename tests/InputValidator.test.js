1c7dc16e09729eb22a6e9edd2b0ae9186659298e
Hilja-Maria Paananen <143746544+h-mp@users.noreply.github.com>
2024-09-29 18:10:14 +0300

    Write tests for temperature range validation

--- tests/InputValidator.test.js
+++ tests/InputValidator.test.js
@@ -32,3 +32,13 @@ test('Validating input type string', () => {
 test('Validating positive number', () => {
   expect(() => inputValidator.validatePositiveNumber(-1)).toThrowError('Number must be positive')
 })
+
+// Test case to check Celsius temperature range validation
+test('Validating Celsius temperature range', () => {
+  expect(() => inputValidator.validateCelsiusRange(-300)).toThrowError('Temperature must be greater than or equal to -273.15°C')
+})
+
+// Test case to check Fahrenheit temperature range validation
+test('Validating Fahrenheit temperature range', () => {
+  expect(() => inputValidator.validateFahrenheitRange(-500)).toThrowError('Temperature must be greater than or equal to -459.67°F')
+})
