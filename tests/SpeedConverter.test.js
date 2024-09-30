a94404fdeb722502029e90281c33dd48aff6cdd0
Hilja-Maria Paananen <143746544+h-mp@users.noreply.github.com>
2024-09-30 12:50:21 +0300

    Write tests for ft/s to m/s conversion

--- tests/SpeedConverter.test.js
+++ tests/SpeedConverter.test.js
@@ -22,9 +22,25 @@ test('Convert kilometers per hour to miles per hour', () => {
   expect(speedConverter.kilometersPerHourToMilesPerHour(200)).toBeCloseTo(124.274, 2)
 })
 
+// Test case to check feet per second to meters per second conversion
+test('Convert feet per second to meters per second', () => {
+  expect(speedConverter.feetPerSecondToMetersPerSecond(60)).toBeCloseTo(18.288, 2)
+
+  expect(speedConverter.feetPerSecondToMetersPerSecond(100)).toBeCloseTo(30.48, 2)
+})
+
+// Test case to check meters per second to feet per second conversion
+test('Convert meters per second to feet per second', () => {
+  expect(speedConverter.metersPerSecondToFeetPerSecond(60)).toBeCloseTo(196.85, 2)
+
+  expect(speedConverter.metersPerSecondToFeetPerSecond(100)).toBeCloseTo(328.08, 2)
+})
+
 // Test case to check input validation
 test('Validate input', () => {
   expect(() => speedConverter.milesPerHourToKilometersPerHour('string')).toThrowError('Input must be a number')
 
   expect(() => speedConverter.kilometersPerHourToMilesPerHour(NaN)).toThrowError('Input must be a number')
+
+  expect(() => speedConverter.feetPerSecondToMetersPerSecond(-10)).toThrowError('Number must be positive')
 })
