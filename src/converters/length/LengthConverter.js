38b317c851d22bf577132904157742ba93b08412
Hilja-Maria Paananen <143746544+h-mp@users.noreply.github.com>
2024-10-09 17:24:28 +0300

    Modify the lengthConverter class to extend Converter

--- src/converters/length/LengthConverter.js
+++ src/converters/length/LengthConverter.js
@@ -4,36 +4,26 @@
  * @author Hilja-Maria Paananen <hp222qn@student.lnu.se>
  */
 
-import { InputValidator } from '../../InputValidator.js'
+import { Converter } from '../Converter.js'
 import { CentimetersToInchesConverter } from './CentimetersToInchesConverter.js'
 import { FeetToMetersConverter } from './FeetToMetersConverter.js'
 import { InchesToCentimetersConverter } from './InchesToCentimetersConverter.js'
 import { MetersToFeetConverter } from './MetersToFeetConverter.js'
 
-export class LengthConverter {
-  #inputValidator
-
-  /**
-   * Unit abbreviation conversions.
-   */
-  #unitAbbreviationConversions
-
-  /**
-   * Available converters for length.
-   */
-  #availableConversions
-
+export class LengthConverter extends Converter {
   constructor() {
-    this.#inputValidator = new InputValidator()
+    super()
 
-    this.#unitAbbreviationConversions = {
+    // The unit abbreviation conversions
+    this._unitAbbreviationConversions = {
       'm': 'meters',
       'ft': 'feet',
       'cm': 'centimeters',
       'in': 'inches'
     }
 
-    this.#availableConversions = {
+    // The available conversions
+    this._availableConversions = {
       "meters-feet": new MetersToFeetConverter(),
       "feet-meters": new FeetToMetersConverter(),
       "centimeters-inches": new CentimetersToInchesConverter(),
@@ -42,62 +32,20 @@ export class LengthConverter {
   }
 
   /**
-   * Validates the number input.
-   * 
-   * @param {*} numberInput - The input number
-   * @throws {Error} - If the input is not a number
-   * @throws {Error} - If the input is not a positive number
-   */
-  #validateNumberInput(numberInput) {
-    this.#inputValidator.validateInputTypeNumber(numberInput)
-    this.#inputValidator.validatePositiveNumber(numberInput)
-  }
-
-  /**
-   * Validates the string inputs.
-   * 
-   * @param {*} convertFromInput - The input string to convert from
-   * @param {*} convertToInput - The input string to convert to
-   * @throws {Error} - If the inputs are not strings
-   */
-  #validateStringInputs(convertFromInput, convertToInput) {
-    this.#inputValidator.validateInputTypeString(convertFromInput)
-    this.#inputValidator.validateInputTypeString(convertToInput)
-  }
-
-  /**
-   * Normalizes the unit abbreviation.
-   * 
-   * @param {string} unitAbbreviation - The unit abbreviation
-   * @returns {string} - The normalized unit name
-   */
-  #normalizeAbbreviation(unitAbbreviation) {
-    return this.#unitAbbreviationConversions[unitAbbreviation.toLowerCase()]
-  }
-
-  /**
    * Handles length conversion selection and converts the number.
    * 
    * @param {String} convertFrom - The unit to convert from
    * @param {String} convertTo - The unit to convert to
    * @param {Number} numberToConvert - The number to convert
-   * @throws {Error} - If the conversion is not available
    * @returns {Number} - The converted number
    */
   convert(convertFrom, convertTo, numberToConvert) {
-    this.#validateNumberInput(numberToConvert)
-    this.#validateStringInputs(convertFrom, convertTo)
+    this._validateNumberInput(numberToConvert)
+    this._validateStringInputs(convertFrom, convertTo)
 
-    const normalizedFrom = this.#normalizeAbbreviation(convertFrom.toLowerCase())
-    const normalizedTo = this.#normalizeAbbreviation(convertTo.toLowerCase())
+    const normalizedFrom = this._normalizeAbbreviation(convertFrom.toLowerCase())
+    const normalizedTo = this._normalizeAbbreviation(convertTo.toLowerCase())
     
-    const key = `${normalizedFrom}-${normalizedTo}`
-    const converter = this.#availableConversions[key]
-
-    if (!converter) {
-      throw new Error('Conversion not available')
-    }
-
-    return converter.convert(numberToConvert)
+    return this._convertValue(normalizedFrom, normalizedTo, numberToConvert)
   }
 }
