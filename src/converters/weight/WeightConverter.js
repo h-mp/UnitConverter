05e5f27f3d1b0b381b1dafdd26add1e36301eaa8
Hilja-Maria Paananen <143746544+h-mp@users.noreply.github.com>
2024-10-10 14:55:51 +0300

    Implement the new classes in eightConverter

--- src/converters/weight/WeightConverter.js
+++ src/converters/weight/WeightConverter.js
@@ -4,37 +4,30 @@
  * @author Hilja-Maria Paananen <hp222qn@student.lnu.se>
  */
 
-import { InputValidator } from '../../InputValidator.js'
+import { Converter } from '../Converter.js'
+import { KilogramsToPoundsConverter } from './KilogramsToPoundsConverter.js'
+import { PoundsToKilogramsConverter } from './PoundsToKilogramsConverter.js'
+import { GramsToOuncesConverter } from './GramsToOuncesConverter.js'
+import { OuncesToGramsConverter } from './OuncesToGramsConverter.js'
 
-export class WeightConverter {
-  #inputValidator
-
-  /**
-   * Conversion rate from kilograms to pounds.
-   */
-  #kgToLbConversionRate
-
-  /**
-   * Conversion rate from kilograms to ounces.
-   */
-  #gToOzConversionRate
+export class WeightConverter extends Converter {
 
   constructor() {
-    this.#inputValidator = new InputValidator()
-    this.#kgToLbConversionRate = 2.2046
-    this.#gToOzConversionRate = 0.035274
-  }
+    super()
 
-  /**
-   * Validates the input.
-   *
-   * @param {*} input - The input
-   * @throws {Error} - If the input is not a number
-   * @throws {Error} - If the input is not a positive number
-   */
-  #validateInput(input) {
-    this.#inputValidator.validateInputTypeNumber(input)
-    this.#inputValidator.validatePositiveNumber(input)
+    this._unitAbbreviationConversions = {
+      "kg": "kilograms",
+      "lb": "pounds",
+      "g": "grams",
+      "oz": "ounces"
+    }
+
+    this._availableConversions = {
+      "kilograms-pounds": new KilogramsToPoundsConverter(),
+      "pounds-kilograms": new PoundsToKilogramsConverter(),
+      "grams-ounces": new GramsToOuncesConverter(),
+      "ounces-grams": new OuncesToGramsConverter()
+    }
   }
 
   /**
@@ -47,88 +40,12 @@ export class WeightConverter {
    * @returns {Number} - The converted number
    */
   convert(convertFrom, convertTo, numberToConvert) {
-    this.#validateInput(numberToConvert)
-    const convertToInLowerCase = convertTo.toLowerCase()
+    this._validateNumberInput(numberToConvert)
+    this._validateStringInputs(convertFrom, convertTo)
 
-    switch (convertFrom.toLowerCase()) {
-      case 'kilograms':
-      case 'kg':
-        switch (convertToInLowerCase) {
-          case 'pounds':
-          case 'lb':
-            return this.#kilogramsToPounds(numberToConvert)
-          default:
-            throw new Error('Conversion not available')
-        }
-      case 'pounds':
-      case 'lb':
-        switch (convertToInLowerCase) {
-          case 'kilograms':
-          case 'kg':
-            return this.#poundsToKilograms(numberToConvert)
-          default:
-            throw new Error('Conversion not available')
-        }
-      case 'ounces':
-      case 'oz':
-        switch (convertToInLowerCase) {
-          case 'grams':
-          case 'g':
-            return this.#ouncesToGrams(numberToConvert)
-          default:
-            throw new Error('Conversion not available')
-        }
-      case 'grams':
-      case 'g':
-        switch (convertToInLowerCase) {
-          case 'ounces':
-          case 'oz':
-            return this.#gramsToOunces(numberToConvert)
-          default:
-            throw new Error('Conversion not available')
-        }
-      default:
-        throw new Error('Conversion not available')
-    }
-  }
+    const normalizedFrom = this._normalizeAbbreviation(convertFrom)
+    const normalizedTo = this._normalizeAbbreviation(convertTo)
 
-  /**
-   * Converts the weight from kilograms to pounds.
-   *
-   * @param {Number} kilograms - The weight to be converted
-   * @returns {Number} - The weight in pounds
-   */
-  #kilogramsToPounds(kilograms) {
-    return kilograms * this.#kgToLbConversionRate
-  }
-
-  /**
-   * Converts the weight from pounds to kilograms.
-   *
-   * @param {Number} pounds - The weight to be converted
-   * @returns {Number} - The weight in kilograms
-   */
-  #poundsToKilograms(pounds) {
-    return pounds / this.#kgToLbConversionRate
-  }
-
-  /**
-   * Converts the weight from grams to ounces.
-   *
-   * @param {Number} grams - The weight to be converted
-   * @returns {Number} - The weight in ounces
-   */
-  #gramsToOunces(grams) {
-    return grams * this.#gToOzConversionRate
-  }
-
-  /**
-   * Converts the weight from ounces to grams.
-   *
-   * @param {Number} ounces - The weight to be converted
-   * @returns {Number} - The weight in grams
-   */
-  #ouncesToGrams(ounces) {
-    return ounces / this.#gToOzConversionRate
+    return this._convertValue(normalizedFrom, normalizedTo, numberToConvert)
   }
 }
