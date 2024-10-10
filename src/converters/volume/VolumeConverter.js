0bd936f394e3fc3de1e717ac79deeda045330e95
Hilja-Maria Paananen <143746544+h-mp@users.noreply.github.com>
2024-10-10 18:11:27 +0300

    Implement the new classes in VolumeConverter

--- src/converters/volume/VolumeConverter.js
+++ src/converters/volume/VolumeConverter.js
@@ -4,43 +4,35 @@
  * @author Hilja-Maria Paananen <hp222qn@student.lnu.se>
  */
 
-import { InputValidator } from '../../InputValidator.js'
+import { Converter } from '../Converter.js'
+import { CupsToDecilitersConverter } from './CupsToDecilitersConverter.js'
+import { DecilitersToCupsConverter } from './DecilitersToCupsConverter.js'
+import { GallonsToLitersConverter } from './GallonsToLitersConverter.js'
+import { LitersToGallonsConverter } from './LitersToGallonsConverter.js'
+import { PintsToLitersConverter } from './PintsToLitersConverter.js'
+import { LitersToPintsConverter } from './LitersToPintsConverter.js'
 
-export class VolumeConverter {
-  #inputValidator
-
-  /**
-   * Conversion rate from gallons to liters.
-   */
-  #galToLConversionRate
-
-  /**
-   * Conversion rate from pints to liters.
-   */
-  #ptToLConversionRate
-
-  /**
-   * Conversion rate from cups to deciliters.
-   */
-  cToDlConversionRate
+export class VolumeConverter extends Converter {
 
   constructor() {
-    this.#inputValidator = new InputValidator()
-    this.#galToLConversionRate = 0.26417
-    this.#ptToLConversionRate = 2.1134
-    this.cToDlConversionRate = 2.3658
-  }
+    super()
 
-  /**
-   * Validates the input.
-   *
-   * @param {*} input - The input
-   * @throws {Error} - If the input is not a number
-   * @throws {Error} - If the input is not a positive number
-   */
-  #validateInput(input) {
-    this.#inputValidator.validateInputTypeNumber(input)
-    this.#inputValidator.validatePositiveNumber(input)
+    this._unitAbbreviationConversions = {
+      "c": "cups",
+      "dl": "deciliters",
+      "gal": "gallons",
+      "l": "liters",
+      "pt": "pints"
+    }
+
+    this._availableConversions = {
+      "cups-deciliters": new CupsToDecilitersConverter(),
+      "deciliters-cups": new DecilitersToCupsConverter(),
+      "gallons-liters": new GallonsToLitersConverter(),
+      "liters-gallons": new LitersToGallonsConverter(),
+      "pints-liters": new PintsToLitersConverter(),
+      "liters-pints": new LitersToPintsConverter()
+    }
   }
 
   /**
@@ -53,120 +45,12 @@ export class VolumeConverter {
    * @returns {Number} - The converted number
    */
   convert(convertFrom, convertTo, numberToConvert) {
-    this.#validateInput(numberToConvert)
-    const convertToInLowerCase = convertTo.toLowerCase()
+    this._validateNumberInput(numberToConvert)
+    this._validateStringInputs(convertFrom, convertTo)
 
-    switch (convertFrom.toLowerCase()) {
-      case 'liters':
-      case 'l':
-        switch (convertToInLowerCase) {
-          case 'gallons':
-          case 'gal':
-            return this.#litersToGallons(numberToConvert)
-          case 'pints':
-          case 'pt':
-            return this.#litersToPints(numberToConvert)
-          default:
-            throw new Error('Conversion not available')
-        }
-      case 'gallons':
-      case 'gal':
-        switch (convertToInLowerCase) {
-          case 'liters':
-          case 'l':
-            return this.#gallonsToLiters(numberToConvert)
-          default:
-            throw new Error('Conversion not available')
-        }
-      case 'pints':
-      case 'pt':
-        switch (convertToInLowerCase) {
-          case 'liters':
-          case 'l':
-            return this.#pintsToLiters(numberToConvert)
-          default:
-            throw new Error('Conversion not available')
-        }
-      case 'cups':
-      case 'c':
-        switch (convertToInLowerCase) {
-          case 'deciliters':
-          case 'dl':
-            return this.#cupsToDeciliters(numberToConvert)
-          default:
-            throw new Error('Conversion not available')
-        }
-      case 'deciliters':
-      case 'dl':
-        switch (convertToInLowerCase) {
-          case 'cups':
-          case 'c':
-            return this.#decilitersToCups(numberToConvert)
-          default:
-            throw new Error('Conversion not available')
-        }
-      default:
-        throw new Error('Conversion not available')
-    }
-  }
+    const normalizedFrom = this._normalizeAbbreviation(convertFrom)
+    const normalizedTo = this._normalizeAbbreviation(convertTo)
 
-  /**
-   *Converts the volume from liters to gallons.
-   *
-   * @param {Number} gallons - The volume to be converted
-   * @return {Number} - The volume in liters
-   */
-  #gallonsToLiters(gallons) {
-    return gallons / this.#galToLConversionRate
-  }
-
-  /**
-   * Converts the volume from liters to gallons.
-   *
-   * @param {Number} liters - The volume to be converted
-   * @return {Number} - The volume in gallons 
-   */
-  #litersToGallons(liters) {
-    return liters * this.#galToLConversionRate
-  }
-
-  /**
-   * Converts the volume from pints to liters.
-   *
-   * @param {Number} pints - The volume to be converted
-   * @return {Number} - The volume in liters
-   */
-  #pintsToLiters(pints) {
-    return pints / this.#ptToLConversionRate
-  }
-
-  /**
-   * Converts the volume from liters to pints.
-   *
-   * @param {Number} liters - The volume to be converted
-   * @return {Number} - The volume in pints
-   */
-  #litersToPints(liters) {
-    return liters * this.#ptToLConversionRate
-  }
-
-  /**
-   * Converts the volume from cups to deciliters.
-   * 
-   * @param {Number} cups - The volume to be converted
-   * @return {Number} - The volume in deciliters
-   */
-  #cupsToDeciliters(cups) {
-    return cups * this.cToDlConversionRate
-  }
-
-  /**
-   * Converts the volume from deciliters to cups.
-   *
-   * @param {Number} deciliters - The volume to be converted
-   * @return {Number} - The volume in cups
-   */
-  #decilitersToCups(deciliters) {
-    return deciliters / this.cToDlConversionRate
+    return this._convertValue(normalizedFrom, normalizedTo, numberToConvert)
   }
 }
